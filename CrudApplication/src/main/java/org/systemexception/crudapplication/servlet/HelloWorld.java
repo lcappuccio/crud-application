e34b168fcadd95802b67666f2eeeb74966587529
Leonardo Cappuccio <leo@systemexception.org>
2015-02-17 23:57:23 +0100

    wip bootstrap

--- CrudApplication/src/main/java/org/systemexception/crudapplication/servlet/HelloWorld.java
+++ CrudApplication/src/main/java/org/systemexception/crudapplication/servlet/HelloWorld.java
@@ -35,6 +35,7 @@ public class HelloWorld extends HttpServlet {
 			out.println("<!DOCTYPE html>");
 			out.println("<html>");
 			out.println("<head>");
+			out.println("<link href=\"css/bootstrap.min.css\" rel=\"stylesheet\">");
 			out.println("<title>Servlet HelloWorld</title>");
 			out.println("</head>");
 			out.println("<body>");
