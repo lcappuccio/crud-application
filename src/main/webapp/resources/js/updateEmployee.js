d9433633261a1721b5f3afd692a852ce618025f4
Leonardo Cappuccio <leo@systemexception.org>
2015-06-02 23:23:36 +0200

    wip button, logging to console

--- src/main/webapp/resources/js/updateEmployee.js
+++ src/main/webapp/resources/js/updateEmployee.js
@@ -1,21 +1,17 @@
 /**
  * Created by leo on 31/05/15.
  */
-$('#updateEmployeeTable tr').each(function () {
-    var customerId = $(this).find(".empID").html();
-    var customerIdRaw = $(this).find("td").eq(1).html();
-    console.log(customerId);
-    console.log(customerIdRaw);
-});
-
 $(document).ready(function(){
     $("button").on('click', handleClick);
 });
+
 function handleClick()
 {
     $(this).siblings().on('click', handleClick);
     $(this).off('click');
     console.log($(this).attr("value"));
-    var id = $(this).closest("tr").find("td").eq(1).text();
-    console.log("Id is " + id);
+    var empId = $(this).closest("tr").find("td:nth-child(1)").text();
+    var firstName = $(this).closest("tr").find("td:nth-child(2)").text();
+    var lastName = $(this).closest("tr").find("td:nth-child(3)").text();
+    console.log("Update " + empId + " to :" + firstName + ", " + lastName);
 }
